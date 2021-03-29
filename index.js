24c40811e3756e23b3f050eab9f8035edbc1d659
Venkatesh Kulkarni <venkateshkulkarni2001@gmail.com>
2021-03-29 16:15:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const bencode = require('bencode');
+const tracker = require('./tracker');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+
+tracker.getPeers(torrent, peers => {
+  console.log('list of peers: ', peers);
+});
